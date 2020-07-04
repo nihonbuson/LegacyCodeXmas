7a9f9eccc745428b9d0e5b41bc17c8eaf3522118
nihonbuson <nihonbuson@gmail.com>
2020-07-04 21:48:36 +0900

    isXmasのテストケースを追加
    しかし、Locale.now()が実装コードにあるため、テストは失敗する

--- src/main/test/HolidayTest.java
+++ src/main/test/HolidayTest.java
@@ -8,4 +8,10 @@ class HolidayTest {
         boolean actual = new Holiday().isXmas();
         assertEquals(false,actual);
     }
+    @Test
+    void isXmas() {
+        boolean actual = new Holiday().isXmas();
+        assertEquals(true,actual);
+    }
+
 }
